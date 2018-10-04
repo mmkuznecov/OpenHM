72dc7d94c22dcd9e1a6d5cc6df570212a56508d2
mmkuznecov <43701555+mmkuznecov@users.noreply.github.com>
2018-10-04 21:18:48 +0300

    Add files via upload

--- hm_class_mapping.py
+++ hm_class_mapping.py
@@ -1,9 +1,17 @@
 import numpy as np
 import cv2
 import copy
+import hm_class_counting
+from imutils.video import VideoStream
+from imutils.video import FPS
+import time
+import dlib
 
 class Mkmap():
 	vidpath=str()
+	outvidpath=str()
+	modelpath=str('MobileNetSSD_deploy.caffemodel')
+	prototxtpath=str('MobileNetSSD_deploy.prototxt.txt')
 	def __init__(self, vidpath):
 		self.vidpath=vidpath
 	def mapping1(self):
@@ -71,6 +79,72 @@ class Mkmap():
 		cap.release()
 		cv2.destroyAllWindows()
 
+	def mapping1rt(self):
+		fps = '30'
+		cap = cv2.VideoCapture(0)
+		#outfile = 'heatmap.mp4'
+
+
+		while True:
+			try:
+				_, f0 = cap.read()
+				f = cv2.resize(f0, (0,0), fx=0.5, fy=0.5) 
+				f = cv2.GaussianBlur(f, (1, 1), 0.5, 0.5)
+				f = cv2.cvtColor(f, cv2.COLOR_BGR2GRAY)
+				cnt = 0
+				res = 0.05*f
+				res = res.astype(np.float64)
+				break
+			except:
+				print('s')
+
+
+		fgbg = cv2.createBackgroundSubtractorMOG2(history=1, varThreshold=500,detectShadows=True)
+
+		#writer = FFmpegWriter(outfile, outputdict={'-r': fps})
+		#writer = FFmpegWriter(outfile)
+
+		kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (13, 13))
+		cnt = 0
+		sec = 0
+		while True:
+			# if sec == duration: break
+			"""cnt += 1
+			if cnt % int(fps) == 0:
+				print(sec)
+				sec += 1"""
+			ret, frame = cap.read()
+			frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5) 
+			if not ret: break
+			fgmask = fgbg.apply(frame, None, 0.3)
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			# if cnt == 30: res
+			gray = cv2.GaussianBlur(gray, (1, 1), 1, 1)
+			gray = gray.astype(np.float64)
+			fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
+			fgmask = fgmask.astype(np.float64)
+			res += (40 * fgmask + gray) * 0.01
+			res_show = res / res.max()
+			res_show = np.floor(res_show * 255)
+			res_show = res_show.astype(np.uint8)
+			res_show = cv2.applyColorMap(res_show, cv2.COLORMAP_JET)
+			cv2.imshow('s', res_show)
+			# if sec < start: continue
+		#	try:
+		#		writer.writeFrame(res_show)
+		#	except:
+		#		writer.close()
+		#		break
+			
+			k = cv2.waitKey(30) & 0xff
+			if k == 27:
+				break
+
+		#writer.close()
+		cap.release()
+		cv2.destroyAllWindows()
+
+
 	def mapping2(self):
 		motionVid=Motion(self.vidpath)
 		motionVid.prep()
@@ -81,11 +155,16 @@ class Mkmap():
 		heatm=HeatMotion(self.vidpath)
 		heatm.detect()
 
+	def countf(self):
+		cn=hm_class_counting.person_count(self.prototxtpath, self.modelpath, self.vidpath, self.outvidpath , 0.4, 30)
+		cn.track()
+		cn.counting()
+
 
 
 class Motion:
 	
-	def __init__(self,vidpath):	
+	def __init__(self, vidpath):	
 					print("Motion Detection Object Created")	
 					#input file name of video
 					self.inname= vidpath
